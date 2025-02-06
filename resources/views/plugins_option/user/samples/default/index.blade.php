7ae8c0c4f6eb85d7f52e0b77f96df992d20beeb6
Mitsuru Mutaguchi <akagane99@gmail.com>
2025-02-06 17:20:20 +0900

    refactor: サンプル, index.balde, バケツありだけ通る用に見直したため、バケツなし処理を削除

--- resources/views/plugins_option/user/samples/default/index.blade.php
+++ resources/views/plugins_option/user/samples/default/index.blade.php
@@ -9,46 +9,34 @@
 @extends('core.cms_frame_base')
 
 @section("plugin_contents_$frame->id")
-
     {{-- 登録後メッセージ表示 --}}
     @include('plugins.common.flash_message_for_frame')
 
-    @if (isset($frame) && $frame->bucket_id)
-        {{-- バケツあり --}}
-
-        {{-- 新規登録 --}}
-        @can('posts.create',[[null, $frame->plugin_name, $buckets]])
-            @if (isset($frame) && $frame->bucket_id)
-                <div class="row">
-                    <p class="text-right col-12">
-                        {{-- 新規登録ボタン --}}
-                        <button type="button" class="btn btn-success" onclick="location.href='{{url('/')}}/plugin/samples/edit/{{$page->id}}/{{$frame_id}}#frame-{{$frame_id}}'">
-                            <i class="far fa-edit"></i> 新規登録
-                        </button>
-                    </p>
-                </div>
-            @endif
-        @endcan
-
-        <div class="row">
-            @foreach($posts as $post_item)
-                <dt class="col-3">
-                    <a href="{{url('/')}}/plugin/samples/show/{{$page->id}}/{{$frame_id}}/{{$post_item->id}}#frame-{{$frame->id}}">
-                        {{$post_item->title}}
-                    </a>
-                </dt>
-                <dd class="col-9">{{$post_item->content}}</dd>
-            @endforeach
-        </div>
-
-        {{-- ページング処理 --}}
-        @include('plugins.common.user_paginate', ['posts' => $posts, 'frame' => $frame, 'aria_label_name' => $frame->id, 'class' => 'mt-3'])
-    @else
-        {{-- バケツなし --}}
-        <div class="card border-danger">
-            <div class="card-body">
-                <p class="text-center cc_margin_bottom_0">フレームの設定画面から、使用するサンプルを選択するか、作成してください。</p>
+    {{-- 新規登録 --}}
+    @can('posts.create',[[null, $frame->plugin_name, $buckets]])
+        @if (isset($frame) && $frame->bucket_id)
+            <div class="row">
+                <p class="text-right col-12">
+                    {{-- 新規登録ボタン --}}
+                    <button type="button" class="btn btn-success" onclick="location.href='{{url('/')}}/plugin/samples/edit/{{$page->id}}/{{$frame_id}}#frame-{{$frame_id}}'">
+                        <i class="far fa-edit"></i> 新規登録
+                    </button>
+                </p>
             </div>
-        </div>
-    @endif
+        @endif
+    @endcan
+
+    <div class="row">
+        @foreach($posts as $post_item)
+            <dt class="col-3">
+                <a href="{{url('/')}}/plugin/samples/show/{{$page->id}}/{{$frame_id}}/{{$post_item->id}}#frame-{{$frame->id}}">
+                    {{$post_item->title}}
+                </a>
+            </dt>
+            <dd class="col-9">{{$post_item->content}}</dd>
+        @endforeach
+    </div>
+
+    {{-- ページング処理 --}}
+    @include('plugins.common.user_paginate', ['posts' => $posts, 'frame' => $frame, 'aria_label_name' => $frame->id, 'class' => 'mt-3'])
 @endsection
